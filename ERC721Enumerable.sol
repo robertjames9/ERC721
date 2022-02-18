9ea926af38b65abc9c7b6495e18d363689974c49
temotee93 <95536550+temotee93@users.noreply.github.com>
2022-02-18 18:06:55 +0800

    Create ERC721Enumerable.sol

---
+++ ERC721Enumerable.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract UberPlasticTestV6 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable {
+    using Counters for Counters.Counter;
+    string public baseExtension = ".json";
+    Counters.Counter private _tokenIdCounter;
+    uint256 public price = 0.06 ether; //mint price
+    uint256 public maxMintAmount = 5; //per address wallet
+    uint256 public maxSupply = 2000; //total supply
+
+    constructor() ERC721("Uber PlasticTestV6", "GGV6") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://gateway.pinata.cloud/ipfs/QmSXPPTLhdMfFENBZ6HgrwYXRaHsMzdEif6C5fco5vKSVj/";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 mintAmount) public payable {
+        uint256 supply = totalSupply();
+        require (mintAmount > 0, "Please enter amount");
+        require (mintAmount <= maxMintAmount, "Maximum mint for 5");
+        require (supply + mintAmount <= maxSupply, "Exceeds max supply");
+        require (msg.value >= price * mintAmount, "No enough ETH, please check the price");
+        
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        for (uint256 i = 1; i <= mintAmount; i++){
+            _safeMint(to, supply + i);
+        }
+        
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+        function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        Strings.toString(tokenId),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+    function setPrice(uint256 _newPrice) public onlyOwner {
+        price = _newPrice;
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success);
+    }
+}
